e544edc6ca469662394bccf04d45a72ba8159830
uriGrif <urielgrif@gmail.com>
2023-12-20 12:55:59 -0300

    readme y theory listos

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ int main()
     std::cout << "Creating Neural Network..." << std::endl;
     NeuralNetwork nn = NeuralNetwork(CATEGORICAL_CROSS_ENTROPY, oneHotEncoder, classificationTesting);
     nn.setHyperParams(1e-2f, 100, 2, (1.f / 255));
+    // for this dataset, each pixel has a value from 0 to 255. With this values, the gradients end up being too high and we have overflow problems. A solution to this, is to divide by 255, so now every pixel goes from 0 to 1
 
     std::cout << "Adding Layers..." << std::endl;
     nn.addLayer(784, 32, RELU);
